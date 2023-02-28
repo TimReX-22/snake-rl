edb7790dd60637e4a63a911eab09856b5d90d3bf
Tim <tim.reinhart@scandit.com>
2023-02-28 16:52:42 +0100

    added parameter json to set agent parameters

--- agent.py
+++ agent.py
@@ -1,47 +1,55 @@
+import json
 from model import Linear_QNet, QTrainer
 from snake_pygame.snake import Direction
 from snake_pygame.snake_game import SnakeGame, Action, State
-import matplotlib.pyplot as plt
+
 import torch
 import random
+import argparse
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+
 from collections import deque
 from typing import List
-import argparse
+from dataclasses import dataclass
 
 import matplotlib
 matplotlib.use('TkAgg')
 
 
-MAX_MEMORY = 100_000
-BATCH_SIZE = 1000
-LR = 0.001
+@dataclass
+class AgentParameters:
+    max_memory: int
+    batch_size: int
+    lr: int
+    gamma: int
+    epsilon: int
+    model_name: str = None
+    train: bool = True
 
 
 class Agent:
-    def __init__(self, model_path: str = None, train: bool = True) -> None:
+    def __init__(self, parameters: AgentParameters) -> None:
         self.n_games = 0
-        self.epsilon = 0  # randomness
-        self.gamma = 0.9  # discount rate [0, 1]
-        self.memory = deque(maxlen=MAX_MEMORY)
+        self.parameters = parameters
+        self.memory = deque(maxlen=self.parameters.max_memory)
         self.model = Linear_QNet(12, 256, 3)
-        if train:
-            self.trainer = QTrainer(model=self.model, lr=LR, gamma=self.gamma)
+        if self.parameters.train:
+            self.trainer = QTrainer(
+                model=self.model, lr=self.parameters.lr, gamma=self.parameters.gamma)
         else:
             self.trainer = None
-        if model_path is not None:
-            self.model.load_state_dict(torch.load(model_path))
-        self.train = train
+            self.model.load_state_dict(torch.load(self.parameters.model_name))
 
     def remember(self, state, action: Action, reward: int, next_state, done: bool) -> None:
         # pop left if MAX_MEMORY is reached
         self.memory.append((state, action, reward, next_state, done))
 
     def train_long_memory(self):
-        if len(self.memory) > BATCH_SIZE:
+        if len(self.memory) > self.parameters.batch_size:
             mini_sample = random.sample(
-                self.memory, BATCH_SIZE)
+                self.memory, self.parameters.batch_size)
         else:
             mini_sample = self.memory
         states, actions, rewards, next_states, dones = zip(*mini_sample)
@@ -52,11 +60,11 @@ class Agent:
 
     def get_action(self, state) -> Action:
         # random moves, tradeoff exploration / exploitation, only during training
-        if self.train:
-            self.epsilon = 100 - self.n_games
+        if self.parameters.train:
+            randomness = self.parameters.epsilon - self.n_games
 
         final_move = [0, 0, 0]
-        if self.train and random.randint(0, 200) < self.epsilon:
+        if self.parameters.train and random.randint(0, 200) < randomness:
             move_idx = random.randint(0, 2)
             final_move[move_idx] = 1
         else:
@@ -76,41 +84,25 @@ def moving_average(x: List[int], size: int) -> int:
     return sum / size
 
 
-def train(checkpoint):
+def train(parameters: AgentParameters):
     plot_scores = []
     plot_mean_scores = []
     plot_games = []
     window_size = 10
     total_score = 0
     record = 0
+
     game = SnakeGame(500, 20)
-    if checkpoint is not None:
-        checkpoint = "./model/" + checkpoint
-
-    agent = Agent(model_path=checkpoint)
-
-    plt.ion()
-
-    figure, ax = plt.subplots(figsize=(10, 8))
-    line1, = ax.plot(plot_games, plot_scores, label="Scores")
-    line2, = ax.plot(plot_games, plot_mean_scores,
-                     label="Moving Average Score")
-    plt.title("Scores", fontsize=20)
-    plt.xlabel("Nr. of Games")
-    plt.ylabel("Score")
+    agent = Agent(parameters)
 
     while True:
         state_old = game.get_state()
-
         final_move: Action = agent.get_action(state_old)
-
         done, score, reward = game.play_step(input_action=final_move)
 
         state_new = game.get_state()
-
         agent.train_short_memory(
             state_old, final_move, reward, state_new, done)
-
         agent.remember(state_old, final_move, reward, state_new, done)
 
         if done:
@@ -128,18 +120,10 @@ def train(checkpoint):
             plot_games.append(agent.n_games)
             plot_mean_scores.append(moving_average(plot_scores, window_size))
 
-            # line1.set_xdata(plot_games)
-            # line1.set_ydata(plot_scores)
-            # line2.set_xdata(plot_games)
-            # line2.set_ydata(plot_mean_scores)
-            # figure.canvas.draw()
-            # figure.canvas.flush_events()
-            # plt.show()
 
-
-def run(model_name: str):
+def run(parameters: AgentParameters):
     game = SnakeGame(500, 20)
-    agent = Agent(model_path=model_name, train=False)
+    agent = Agent(parameters)
     done = False
     record: int = 0
 
@@ -160,31 +144,32 @@ def run(model_name: str):
                 done = False
 
 
+def get_parameters(filename: str) -> AgentParameters:
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    if "model_name" in data:
+        return AgentParameters(data["max_memory"], data["batch_size"],
+                               data["lr"], data["gamma"], data["epsilon"],
+                               data["model_name"], train=False)
+    return AgentParameters(data["max_memory"], data["batch_size"],
+                           data["lr"], data["gamma"], data["epsilon"])
+
+
 def cli() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description='Command line Interface for the Snake-Agent')
-    parser.add_argument('-t', '--train', action='store_true',
-                        help='train a new agent')
-    parser.add_argument('-m', '--model', required=False,
-                        help="name of model to run, in folder ./model")
-    parser.add_argument('-c', '--checkpoint', required=False,
-                        help="name of checkpoint to continue training, in folder ./model")
-    parser.set_defaults(train=False)
+    parser.add_argument('parameter_filename')
     args = parser.parse_args()
     return args
 
 
 if __name__ == "__main__":
     args = cli()
-    if (args.train):
-        checkpoint = None
-        if args.checkpoint:
-            checkpoint = args.checkpoint
-        train(checkpoint)
-    elif (args.model):
-        if os.path.isfile("./model/" + args.model):
-            run("./model/" + args.model)
+    params = get_parameters(args.parameter_filename)
+    if (params.train):
+        train(params)
+    else:
+        if os.path.isfile(params.model_name):
+            run(params)
         else:
             print("Model file does not exist!")
-    else:
-        print("Failure: Eiher specifiy training or which model to run! Aborting...")
